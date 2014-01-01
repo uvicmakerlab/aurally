88c23e1a1134ead2ba88433c2a417ea0c26ae233
Keeler Russell <keeler.russell@gmail.com>
2013-12-31 21:50:30 -0600

    Beginning of Wav file handler.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Aurally
 
 Defn: With regard to sound or the ear; e.g. "the new musical was visually and aurally appealing".
+
+## Dependencies
+
+SciPy Stack - http://www.scipy.org/install.html
---
+++ wav.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+from scipy.io import wavfile
+
+class Wav:
+    """
+    Wrapper for operations on .wav files.
+    """
+
+    def __init__(self, filepath):
+        """"
+        Loads the .wav file from the given filepath.
+
+        filepath should not be None, whitespace, or empty.
+        """
+        self.load(filepath)
+
+    def load(self, filepath):
+        """
+        Loads the .wav file from the given filepath.
+
+        filepath should not be None, whitespace, or empty.
+        """
+
+        assert filepath is not None, 'Filename cannot be None.'
+        assert filepath.strip() != '', 'Filename cannot be empty or whitespace.'
+
+        filename, extension = os.path.splitext(filepath)
+        assert extension.lower() == '.wav', 'Cannot load files with \'{0}\' extension.'.format(extension)
+        
+        self.sample_rate, self.data = wavfile.read(filepath)
+
+if __name__ == '__main__':
+    help(Wav)
+ 
